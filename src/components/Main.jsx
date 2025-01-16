8af2e90a6d618557aefe0f2e2b56ba9b97ae6b86
Carlos J. Ramirez <carlosjramirez11@gmail.com>
2025-01-16 10:26:15 -0800

    Styled the main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,14 @@
 export default function Main() {
-  return <h1>Main component here</h1>;
+  return (
+    <main>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Is maintained by Meta</li>
+        <li>Has well over 200K starts on GitHub</li>
+        <li>Power thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
 }
